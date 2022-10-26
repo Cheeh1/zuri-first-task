991a3c4379f3dfafa3d9cbbc37d54656fb2b3746
Divine Edwin <edwindivine5@gmail.com>
2022-10-26 09:54:02 +0100

    links updated
    All link tags has been fixed with their proper links and soical icons has been included.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import pic from './assets/profileImg.png'
+import {github, slack, share, zuri, logo} from './assets'
 import './App.css'
 
 function App() {
@@ -8,16 +9,21 @@ function App() {
      <header className='profile'>
         <img className='profile-pic' id='profile__img' src={pic} alt="profile-pic" />
         <p className='profile-name'id='twitter'>@iamcheeh</p>
-        <p className='profile-name-2'id='slack'>iamcheeh</p>
+        <p className='profile-name-2'id='slack'>divineedwin</p>
      </header>
 
     <section className='links'>
-      <a className='link-tw' href="http://twitter.com/iamcheeh">Twitter Link</a>
-      <a className='link-tw' href="http://">Zuri Team</a>
-      <a className='link-tw' href="http://">Zuri Books</a>
-      <a className='link-tw' href="http://">Python Books</a>
-      <a className='link-tw' href="http://">Background Checks for Coders</a>
-      <a className='link-tw' href="http://">Design Books</a>
+      <a href="http://twitter.com/iamcheeh" target="_blank"><p className='link-txt'>Twitter Links </p></a>
+      <a id='btn__zuri' href="https://training.zuri.team/" target="_blank"><p className='link-txt'>Zuri Team</p></a>
+      <a id='books' href="http://books.zuri.team" target="_blank"><p className='link-txt'>Zuri Books</p></a>
+      <a id='book__python' href="https://books.zuri.team](https://books.zuri.team)/python-for-beginners?ref_id=<divineedwin>" target="_blank"><p className='link-txt'>Python Books</p></a>
+      <a id='pitch' href="https://background.zuri.team/" target="_blank"><p className='link-txt'>Background Checks for Coders</p></a>
+      <a id='book__design' href="https://books.zuri.team/design-rules" target="_blank"><p className='link-txt'>Design Books</p></a>
+
+      <div className='social-icon'>
+        <img src={slack} alt="slack-icon" />
+        <img src={github} alt="github-icon" />
+      </div>
     </section>
     </div>
   )
---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import github from './github.png';
+import slack from './slack.png';
+import share from './share.png';
+import zuri from './zuri.png';
+import logo from './I4G.png';
+
+export {github, slack, share, zuri, logo};
