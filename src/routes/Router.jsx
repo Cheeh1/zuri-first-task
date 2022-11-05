3ccc2c6e7902bc7e1c5fe41c6d243b71ccb432a0
Divine Edwin <edwindivine5@gmail.com>
2022-11-05 22:50:07 +0100

    Adjusted Routed link

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -9,7 +9,7 @@ function RouterLink() {
       <Router>
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='contact' element={<Contact />} />
+            <Route path='/contact' element={<Contact />} />
         </Routes>
       </Router>
     );
